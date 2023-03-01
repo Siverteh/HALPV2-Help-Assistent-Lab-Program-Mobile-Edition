009592c949c2b013d6743ae4d0652eb957f9d45d
Charlotte Celine Thorjussen <charlottct@student.uia.no>
2023-03-01 14:01:45 +0100

    ticket and queue

---
+++ src/features/Queue.tsx
@@ -0,0 +1,43 @@
+import Button from "@mui/material/Button"
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
+
+const Queue = () => {
+
+    const handleEdit = () => {
+
+    }
+
+    const handleCancel = () => {
+
+    }
+
+    return (
+        <Box sx={{ borderRadius: 1 }} alignItems='center'>
+            <Typography mt={2} variant="h4">
+                Hi
+            </Typography>
+
+            <Typography mt={2} variant="h6">
+                You are number
+            </Typography>
+
+            <Typography mt={1} variant="h1">
+                838
+            </Typography>
+
+            <Typography mt={1} variant="h6">
+                in the queue
+            </Typography>
+
+            <Button onClick={handleEdit}>
+                Edit ticket
+            </Button>
+            <Button onClick={handleCancel}>
+                Cancel ticket
+            </Button>
+        </Box>
+    )
+}
+
+export default Queue
