df6d0397d54b08bde3df17f0ace7144da54f9284
Sondre Horpestad <sondreho@uia.no>
2023-03-21 10:59:09 +0100

    CHANV2-31/Settings_GUI Shortened a line

--- src/features/Settings.tsx
+++ src/features/Settings.tsx
@@ -25,15 +25,17 @@ const Roles = () => (
   <View/>
 );
 
-const renderTabBar = (props: JSX.IntrinsicAttributes & SceneRendererProps & { navigationState: NavigationState<Route>; scrollEnabled?: boolean | undefined; bounces?: boolean | undefined; activeColor?: string | undefined; inactiveColor?: string | undefined; pressColor?: string | undefined; pressOpacity?: number | undefined; getLabelText?: ((scene: Scene<Route>) => string | undefined) | undefined; getAccessible?: ((scene: Scene<Route>) => boolean | undefined) | undefined; getAccessibilityLabel?: ((scene: Scene<Route>) => string | undefined) | undefined; getTestID?: ((scene: Scene<Route>) => string | undefined) | undefined; renderLabel?: ((scene: Scene<Route> & { focused: boolean; color: string; }) => React.ReactNode) | undefined; renderIcon?: ((scene: Scene<Route> & { focused: boolean; color: string; }) => React.ReactNode) | undefined; renderBadge?: ((scene: Scene<Route>) => React.ReactNode) | undefined; renderIndicator?: ((props: TabBarIndicatorProps<Route>) => React.ReactNode) | undefined; renderTabBarItem?: ((props: TabBarItemProps<Route> & { key: string; }) => React.ReactElement<any, string | React.JSXElementConstructor<any>>) | undefined; onTabPress?: ((scene: Scene<Route> & Event) => void) | undefined; onTabLongPress?: ((scene: Scene<Route>) => void) | undefined; tabStyle?: StyleProp<ViewStyle>; indicatorStyle?: StyleProp<ViewStyle>; indicatorContainerStyle?: StyleProp<ViewStyle>; labelStyle?: StyleProp<TextStyle>; contentContainerStyle?: StyleProp<ViewStyle>; style?: StyleProp<ViewStyle>; gap?: number | undefined; testID?: string | undefined; android_ripple?: PressableAndroidRippleConfig | undefined; }) => (
-  <TabBar
-    {...props}
-    indicatorStyle={{ backgroundColor: 'black' }}
-    style={{...Styles.lm_background, paddingTop: 40}}
-    labelStyle={{ ...Styles.lm_text }}
-    pressColor='#E0EEF7'
-  />
-);
+const renderTabBar = (props:any) => {
+  return (
+    <TabBar
+      {...props}
+      indicatorStyle={{ backgroundColor: 'black' }}
+      style={{...Styles.lm_background, paddingTop: 40}}
+      labelStyle={{ ...Styles.lm_text }}
+      pressColor='#E0EEF7'
+    />
+  );
+};
 
 export default class Tabs extends React.Component {
   state = {
