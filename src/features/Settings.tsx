8286ce6b27e6b22a2024464fa8be20842897d381
Sondre Horpestad <sondreho@uia.no>
2023-03-30 10:59:43 +0200

    CHANV2-41/Logic-Settings The settingspage have the feture it is supposed to have. Still need to add the api-calls and getting the user that is loged in

--- src/features/Settings.tsx
+++ src/features/Settings.tsx
@@ -4,80 +4,103 @@ import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 import { Dimensions, useColorScheme, View} from 'react-native';
 import * as React from 'react';
 
+const Text_Input = ({isDarkMode}: {isDarkMode: boolean}, lable:string, defaultValue:string = '', password:boolean = false) => {
+  return(
+    <>
+      <View style={{height:'7%'}}></View>
+      <TextInput 
+      style={{width: "80%"}}
+              textColor={isDarkMode ? '#FFFFFF' : '#201C24'}
+              activeOutlineColor = {isDarkMode ? '#FFFFFF' : '#201C24'}
+              outlineColor = {isDarkMode ? '#0070C0' : '#201C24'}
+              theme={{ colors: { background: isDarkMode ? '#0070C0' : '#FFFFFF',
+              onSurfaceVariant: isDarkMode ? '#FFFFFF' : '#201C24' } }}
+              label={lable}
+              mode="outlined"
+              defaultValue={defaultValue}
+              secureTextEntry={password}>
+    </TextInput>
+  </>
+  )
+}
+
+
+const Button_Settings = ({isDarkMode}: {isDarkMode: boolean}, Value:string, onPress:any) => {
+  return(
+    <>
+      <Button style={[isDarkMode ? Styles.dm_button : Styles.lm_button, {height: "8%", width:"60%"}]}
+          mode="contained"
+          textColor={isDarkMode ? "#FFFFFF" : "#201C24"}
+          contentStyle={{flexDirection: 'row-reverse', height: "100%", width: "100%"}}
+          onPress={onPress}>
+          {Value}
+      </Button>
+      <View style={{height:"5%"}}></View>
+    </>
+  )
+}
+
+const Button_Modal = ({isDarkMode}: {isDarkMode: boolean}, Value:string, onPress:any, Height:string = '15%' ) => {
+  return(
+    <>
+      <View style={{height:"10%"}}></View>
+      <Button style={[isDarkMode ? Styles.dm_button : Styles.lm_button, {height: Height, width:"60%"}]}
+          mode="contained"
+          textColor={isDarkMode ? "#FFFFFF" : "#201C24"}
+          contentStyle={{flexDirection: 'row-reverse', height: "100%", width: "100%"}}
+          onPress={onPress}>
+          {Value}
+      </Button>
+    </>
+  )
+}
+
 const Settings = ({isDarkMode}: {isDarkMode: boolean}) => {
   const [isProfileModalVisible, setIsProfileModalVisible] = React.useState(false);
-
   const openProfileModal = () => setIsProfileModalVisible(true);
   const closeProfileModal = () => setIsProfileModalVisible(false);
+
+  const [isPasswordModalVisible, setIsPasswordModalVisible] = React.useState(false);
+  const openPasswordModal = () => setIsPasswordModalVisible(true);
+  const closePasswordModal = () => setIsPasswordModalVisible(false);
+
+  const [isExserviceModalVisible, setIsExserviceModalVisible] = React.useState(false);
+  const openExserviceModal = () => setIsExserviceModalVisible(true);
+  const closeExserviceModal = () => setIsExserviceModalVisible(false);
+
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = React.useState(false);
+  const openDeleteModal = () => setIsDeleteModalVisible(true);
+  const closeDeleteModal = () => setIsDeleteModalVisible(false);
+
   const screenHeight = Dimensions.get("window").height;
-  const containerStyle = [isDarkMode ? Styles.dm_background : Styles.lm_background, {height: screenHeight*0.4, width: "70%", borderRadius: 20}];
+  const containerStyle = [isDarkMode ? Styles.dm_background : Styles.lm_background, {height: screenHeight*0.45, width: "70%", borderRadius: 20}];
   
 
   return (
     <View style={[isDarkMode ? Styles.dm_background: Styles.lm_background, {justifyContent: 'center', alignItems: 'center', height: screenHeight*0.70 }]}>
-      <Button style={[isDarkMode ? Styles.dm_button : Styles.lm_button, {height: "8%", width:"60%"}]}
-        mode="contained"
-        textColor={isDarkMode ? "#FFFFFF" : "#201C24"}
-        contentStyle={{flexDirection: 'row-reverse', height: "100%", width: "100%"}}
-        onPress={openProfileModal}>
-        PROFILE
-      </Button>
-      <View style={{height:"5%"}}></View>
-      <Button style={[isDarkMode ? Styles.dm_button : Styles.lm_button, {height: "10%", width:"60%"}]}
-        mode="contained"
-        textColor={isDarkMode ? "#FFFFFF" : "#201C24"}
-        contentStyle={{flexDirection: 'row-reverse', height: "100%", width: "100%"}}
-        onPress={() => console.log("hei")}>
-        CHANGE PASSWORD
-      </Button>
-      <View style={{height:"5%"}}></View>
-      <Button style={[isDarkMode ? Styles.dm_button : Styles.lm_button, {height: "10%", width:"60%"}]}
-        mode="contained"
-        textColor={isDarkMode ? "#FFFFFF" : "#201C24"}
-        contentStyle={{flexDirection: 'row-reverse', height: "100%", width: "100%"}}
-        onPress={() => console.log("hei")}>
-        EXTERNAL SERVICES
-      </Button>
-      <View style={{height:"5%"}}></View>
-      <Button style={[isDarkMode ? Styles.dm_button : Styles.lm_button, {height: "10%", width:"60%"}]}
-        mode="contained"
-        textColor={isDarkMode ? "#FFFFFF" : "#201C24"}
-        contentStyle={{flexDirection: 'row-reverse', height: "100%", width: "100%"}}
-        onPress={() => console.log("hei")}>
-        DELETE PERSONAL DATA
-      </Button>
+      {Button_Settings({isDarkMode}, "PROFILE", openProfileModal)}
+      {Button_Settings({isDarkMode}, "PASSWORD", openPasswordModal)}
+      {Button_Settings({isDarkMode}, "EXTERNAL-SERVICE", openExserviceModal)}
+      {Button_Settings({isDarkMode}, "DELETE ACCOUNT", openDeleteModal)}
 
       <Portal>
         <Modal visible={isProfileModalVisible} onDismiss={closeProfileModal} contentContainerStyle={[containerStyle, {alignSelf: 'center', alignItems: 'center', opacity: 0.8}]}>
-        <TextInput 
-            style={{width: "80%", marginTop: "10%"}}
-            textColor={isDarkMode ? '#FFFFFF' : '#201C24'}
-            activeOutlineColor = {isDarkMode ? '#FFFFFF' : '#201C24'}
-            outlineColor = {isDarkMode ? '#0070C0' : '#201C24'}
-            theme={{ colors: { background: isDarkMode ? '#0070C0' : '#FFFFFF',
-                              onSurfaceVariant: isDarkMode ? '#FFFFFF' : '#201C24' } }}
-            label="Name"
-            mode="outlined"
-            defaultValue="John Doe">
-          </TextInput>
-          <TextInput 
-            style={{width: "80%", marginTop: "10%"}}
-            textColor={isDarkMode ? '#FFFFFF' : '#201C24'}
-            activeOutlineColor = {isDarkMode ? '#FFFFFF' : '#201C24'}
-            outlineColor = {isDarkMode ? '#0070C0' : '#201C24'}
-            theme={{ colors: { background: isDarkMode ? '#0070C0' : '#FFFFFF',
-                              onSurfaceVariant: isDarkMode ? '#FFFFFF' : '#201C24' } }}
-            label="Discord"
-            mode="outlined"
-            defaultValue="John Doe#2345">
-          </TextInput>
-          <Button style={[isDarkMode ? Styles.dm_button : Styles.lm_button, {height: "15%", width:"60%", marginTop: "10%"}]}
-            mode="contained"
-            textColor={isDarkMode ? "#FFFFFF" : "#201C24"}
-            contentStyle={{flexDirection: 'row-reverse', height: "100%", width: "100%"}}
-            onPress={() => console.log("hei")}>
-            SAVE
-          </Button>
+          {Text_Input({isDarkMode}, "Name", "Doe")}
+          {Text_Input({isDarkMode}, "Discord", "Doe#1234")}
+          {Text_Input({isDarkMode}, "Email", "Doe@uia.no")}
+          {Button_Modal({isDarkMode}, "SAVE", closeProfileModal)}
+        </Modal>
+        <Modal visible={isPasswordModalVisible} onDismiss={closePasswordModal} contentContainerStyle={[containerStyle, {alignSelf: 'center', alignItems: 'center', opacity: 0.8}]} >
+          {Text_Input({isDarkMode}, "Old Password", '', true)}
+          {Text_Input({isDarkMode}, "New Password", '', true)}
+          {Text_Input({isDarkMode}, "Confirm Password", '', true)}
+          {Button_Modal({isDarkMode}, "SAVE", closePasswordModal)}
+        </Modal>
+        <Modal visible={isExserviceModalVisible} onDismiss={closeExserviceModal} contentContainerStyle={[containerStyle, {alignSelf: 'center', alignItems: 'center', opacity: 0.8, height: screenHeight*0.20}]} >
+          {Button_Modal({isDarkMode}, "CONECT DISCORD", closeExserviceModal, '30%')}
+        </Modal>
+        <Modal visible={isDeleteModalVisible} onDismiss={closeDeleteModal} contentContainerStyle={[containerStyle, {alignSelf: 'center', alignItems: 'center', opacity: 0.8, height: screenHeight*0.20}]} >
+          {Button_Modal({isDarkMode}, "DELETE ACCOUNT", closeDeleteModal, '30%')}
         </Modal>
       </Portal>
     </View>
