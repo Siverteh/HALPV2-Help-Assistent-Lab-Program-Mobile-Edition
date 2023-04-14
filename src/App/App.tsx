6690e92f0f82679a8e6a25860cd817dbbd6b0910
Charlotte Celine Thorjussen <charlottct@student.uia.no>
2023-04-14 14:43:00 +0200

    CHANV2-26/navigation-bar-2 removed some not used coded

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,59 +1,20 @@
 import React, { useMemo, useState } from "react";
-import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 
 import {
-  MD3DarkTheme as PaperDarkTheme,
-  DefaultTheme as PaperLightTheme,
   Provider as PaperProvider,
 } from 'react-native-paper';
 import NavigationBar from "../Components/NavigationBar/NavigationBar";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions
-} from "react-native/Libraries/NewAppScreen";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import Styles from "../styles/styles";
-
-import CreateTicket from "../features/Ticket";
-import Queue from "../features/Queue";
-import Tabs from "../features/Settings";
-import Login from "../features/Login";
-import Helplist from "../features/Helplist";
-import Archive from "../features/Archive";
-import Register from "../features/Register"
-import LabQueues from "../features/LabQueues";
-import { Text, View } from 'react-native';
-
 function App(): JSX.Element {
-  const [theme, setTheme] = useState(DefaultTheme);
-
-  const paperTheme = useMemo(() => {
-    const t = theme.dark ? PaperDarkTheme : PaperLightTheme;
-
-    return {
-      ...t,
-      colors: {
-        ...t.colors,
-        ...theme.colors,
-        surface: theme.colors.card,
-        accent: theme.dark ? 'rgb(255, 55, 95)' : 'rgb(255, 45, 85)',
-      },
-    };
-  }, [theme.colors, theme.dark]);
-  
   return (
-      <PaperProvider theme={paperTheme}>
+      // <PaperProvider> // theme={paperTheme}>
+      <PaperProvider>
         <NavigationContainer>
           <NavigationBar isStudass={true}/>
         </NavigationContainer>
         </PaperProvider>
     );
 }
-
 
 export default App;
