645176de1099c71684f1a3b54bb0dde8a594a953
Sivert Espeland Husebø <siverteh@uia.no>
2023-04-17 13:56:49 +0200

    CHANV2-58/Forgotten-Password Added forgotten password screen design

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,8 +3,6 @@ import { Provider as PaperProvider } from "react-native-paper";
 import NavigationBar from "../Components/NavigationBar/NavigationBar";
 import { NavigationContainer } from '@react-navigation/native';
 
-import ForgottenPassword from "../features/ForgottenPassword";
-
 function App(): JSX.Element {
 
   return (
